bf775b95f8dc52187914306ac6166749a3879ad4
Vedang1102 <atgurvedang@gmail.com>
2024-09-20 00:29:38 +0530

    Setup model

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import './style.css'
+import { OrbitControls } from '@react-three/drei'
+import MacContainer from './MacContainer'
 
 const App = () => {
   return (
-    <Canvas>
-      <mesh>
-        <boxGeometry/>
-      </mesh>
+    <Canvas camera={{fov:20, position:[0, -2, 120]}}>
+      <MacContainer />
+      <OrbitControls />
     </Canvas>
   )
 }
