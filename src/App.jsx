f02f9e20a712ef2dfed114be4bca6b6f1947fad1
Vedang1102 <atgurvedang@gmail.com>
2024-09-21 00:36:55 +0530

    Screen is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import './style.css'
-import { Environment, OrbitControls } from '@react-three/drei'
+import { ScrollControls, Environment, OrbitControls } from '@react-three/drei'
 import MacContainer from './MacContainer'
 
 const App = () => {
   return (
-    <Canvas camera={{fov:20, position:[0, -2, 120]}}>
-      <MacContainer />
-      <OrbitControls />
+    <Canvas camera={{fov:12, position:[0, -10, 220]}}>
       <Environment
       files={[
         "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/studio_small_09_2k.hdr",
       ]} />
+      <ScrollControls pages={3}>
+        <MacContainer />
+      </ScrollControls>
     </Canvas>
   )
 }
